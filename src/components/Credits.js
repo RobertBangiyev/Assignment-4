1c20925a70931918b430d257118b8e26f96cfc59
Mohammad Sarker <sarkerm948@gmail.com>
2022-04-16 22:21:02 -0400

    added styling and classnames to credits

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Credits extends Component {
   constructor(props) {
@@ -14,36 +14,46 @@ class Credits extends Component {
   creditsView = () => {
     const { credits } = this.props;
     return credits.map((credit) => {
-      let date = credit.date.slice(0,10);
+      let date = credit.date.slice(0, 10);
       return <li key={credit.id}>{credit.amount} {credit.description} {date}</li>
-    }) 
+    })
   }
 
   changeDescription = (e) => {
     const descriptionText = e.target.value;
-    this.setState({description: descriptionText});
+    this.setState({ description: descriptionText });
   }
 
   changeAmount = (e) => {
     const amountNum = e.target.value;
-    this.setState({amount: amountNum});
+    this.setState({ amount: amountNum });
   }
 
   render() {
     return (
       <div>
-        <h1>Credits</h1>
-        {this.creditsView()}
-        <form onSubmit={(e) => this.props.addCredit(e, this.state.description, this.state.amount)}>
-          <input type="text" name="description" value={this.state.description} onChange={this.changeDescription}/>
-          <input type="number" name="amount" value={this.state.amount} onChange={this.changeAmount}/>
-          <button type="submit">Add Credit</button>
-        </form>
-        <p>Account Balance: {this.props.accountBalance}</p>
-        <Link to="/">Return to Home</Link>
-      </div>
-    )
+        <nav className='navBar'>
+          <Link to="/login">Login</Link>
+          <br />
+          <Link to="/">Home</Link>
+          <br />
+          <Link to="/userProfile">User Profile</Link>
+          <br />
+          <Link to="/debits">Debits</Link>
+        </nav>
+          <h1>Credits</h1>
+          <div className='dcContainer'>
+          {this.creditsView()}
+          </div>
+          <form onSubmit={(e) => this.props.addCredit(e, this.state.description, this.state.amount)}>
+            <input className='textField' type="text" name="description" placeholder="Description" value={this.state.description} onChange={this.changeDescription} />
+            <input className='textField' type="number" name="amount" placeholder="Amount" value={this.state.amount} onChange={this.changeAmount} />
+            <button id='loginBtn' type="submit">Add Credit</button>
+          </form>
+          <p>Account Balance: {this.props.accountBalance}</p>
+        </div>
+        )
   }
 }
 
-export default Credits;
+        export default Credits;
